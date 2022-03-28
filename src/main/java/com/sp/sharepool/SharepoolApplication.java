1c6752c3e412ba55a4adf08c6ea640970093a05f
hugolind <hugolindstedt@outlook.com>
2022-03-28 14:19:52 +0200

    First Push

---
+++ src/main/java/com/sp/sharepool/SharepoolApplication.java
@@ -0,0 +1,13 @@
+package com.sp.sharepool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharepoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharepoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sp/sharepool/SharepoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.sharepool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharepoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
