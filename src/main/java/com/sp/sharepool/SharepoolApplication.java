a4111799e4789a034d1ad6b9a8be23d140d02f07
ElisabetAronsson <96416333+ElisabetAronsson@users.noreply.github.com>
2022-03-28 15:34:32 +0200

    Lade till textfil!

--- src/main/java/com/sp/sharepool/SharepoolApplication.java
+++ src/main/java/com/sp/sharepool/SharepoolApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SharepoolApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SharepoolApplication.class, args);
 	}
-
 }
